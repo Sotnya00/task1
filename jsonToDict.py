59b0cf64b4bb053a41eddb34c55083117bc967e8
AnthonyDem <sickanthony666>
2021-07-13 10:41:32 +0300

    correct task

--- jsonToDict.py
+++ jsonToDict.py
@@ -1,9 +1,21 @@
 import json
+from dicttoxml import dicttoxml
+from xml.dom.minidom import parseString
 
-with open('students.json', 'r+', encoding = 'utf=8') as student:
-    data = json.load(student)
-print(data)
+class FileHandler:
+    @staticmethod
+    def load_data(path):
+        with open(path, 'r+', encoding = 'utf=8') as student:
+            data = json.load(student)
+        return data
 
-with open('rooms.json', 'r+', encoding='utf-8') as rooms:
-    data1 = json.load(rooms)
-print(data1)
+    @staticmethod
+    def export_json(data):
+        with open('output.json','w') as stud_in_json:
+            stud_in_json.write(json.dumps(data))
+
+    @staticmethod
+    def export_xml(data):
+        out = parseString(dicttoxml(data)).toprettyxml()
+        with open('output.xml', 'w') as stud_in_xml:
+            stud_in_xml.write(str(out))
--- taskAnton.py
+++ taskAnton.py
@@ -1,11 +1,37 @@
 import argparse
-from jsonToDict import *
+from jsonToDict import FileHandler
+
 
 def related_data(rooms, students):
-    rel_dict = {room['id'] : {'name' : room['name'], 'students':[]} for room in rooms }
+    rel_dict = {room['id']: {'name': room['name'], 'students': []} for room in rooms}
     for student in students:
         room_for_student = rel_dict.get(student.get('room'))
         if room_for_student is not None:
             room_for_student.get('students').append(student)
-    data2 = [{'id' : i[0], **i[1]} for i in rel_dict.items()]
+    data2 = [{'id': i[0], **i[1]} for i in rel_dict.items()]
     return data2
+
+
+def arg_pars():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('path1')
+    parser.add_argument('path2')
+    parser.add_argument('format')
+    return parser
+
+
+def main(path_students, path_rooms, format):
+    handler = FileHandler()
+    students = handler.load_data(path_students)
+    rooms = handler.load_data(path_rooms)
+    data_conv = related_data(rooms, students)
+    if format == 'json':
+        handler.export_json(data_conv)
+    elif format == 'xml':
+        handler.export_xml(data_conv)
+
+
+if __name__ == '__main__':
+    parser = arg_pars()
+    args = parser.parse_args()
+    main(args.path1, args.path2, args.format)
